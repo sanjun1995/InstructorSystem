6a044b5ff860bf93f42a068726254126f15ef27a
sanjun <593721042@qq.com>
2019-03-05 00:06:16 +0800

    20190305

---
+++ backend/copyinstructor/src/main/java/cn/instructorsystem/sysmanager/CopyInstructorApplication.java
@@ -0,0 +1,12 @@
+package cn.instructorsystem.sysmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopyInstructorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CopyInstructorApplication.class, args);
+    }
+}
---
+++ backend/copyinstructor/src/main/java/cn/instructorsystem/sysmanager/controller/LoginController.java
@@ -0,0 +1,40 @@
+package cn.instructorsystem.sysmanager.controller;
+
+import cn.instructorsystem.sysmanager.model.SysManager;
+import cn.instructorsystem.sysmanager.model.res.ResponseResult;
+import cn.instructorsystem.sysmanager.service.SysManagerService;
+import cn.instructorsystem.sysmanager.util.ResponseCode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/sysManager")
+public class LoginController {
+
+    private static Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+    @Autowired
+    private SysManagerService sysManagerService;
+
+    @RequestMapping(value = "/login")
+    public ResponseResult<SysManager> login(SysManager student) {
+        boolean isLogin = sysManagerService.login(student);
+        ResponseResult<SysManager> res = new ResponseResult<>();
+        // 清空密码
+        student.setPassword(null);
+        res.setData(student);
+        if (isLogin) {
+            res.setCode(ResponseCode.SUCCESS);
+            res.setMsg("登录成功!");
+            logger.info("sysmanager.login() account: {}，登录成功！", student.getAccount());
+        } else {
+            res.setCode(ResponseCode.FAILURE);
+            res.setMsg("账号或密码错误!");
+            logger.info("sysmanager.login() account: {}，登录失败！", student.getAccount());
+        }
+        return res;
+    }
+}
